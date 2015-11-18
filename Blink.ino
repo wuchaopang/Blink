47762419567657081d99aa64858e0aa98c6bdf07
wuchaopang <20141105049wch@mac23.local>
2015-11-18 20:47:08 +0800

    LED
    让LED按一个频率闪烁

--- Blink.ino
+++ Blink.ino
@@ -26,9 +26,5 @@ void loop() {
   delay(100);              // wait for a second
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(300);              // wait for a second
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(700); 
- 
+  
 }
